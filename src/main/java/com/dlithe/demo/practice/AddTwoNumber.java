b6f7e2013e4d5623308332347ca2fdb16a3c89d0
swapnamk2618 <swapnakohrekar@gmail.com>
2022-04-26 23:04:44 +0530

    Here i am created one AddTwonumber class and write the logic

---
+++ src/main/java/com/dlithe/demo/practice/AddTwoNumber.java
@@ -0,0 +1,16 @@
+package com.dlithe.demo.practice;
+
+/**
+ * @author Swapna Kochrekar
+ * @version 1.0
+ * @
+ */
+
+public class AddTwoNumber {
+
+    public void addNumber(int a, int b){
+        a=20;
+        b=40;
+        System.out.println(a+b);
+    }
+}
